afb14ef99a5a6b2b23b2d0d44c7eeff1dba03d35
open-cover <admin@opencover.net>
2026-06-06 20:41:14 -0600

    cont

--- tests/log.py
+++ tests/log.py
@@ -1,3 +1,5 @@
+from src.battle import PSBattle, PSPlayer
+
 import oak
 import oak.log
 
@@ -26,19 +28,33 @@ def get_random_battle() -> oak.Battle:
 def rollout_random_battle_with_log():
     battle = get_random_battle()
     durations = oak.Durations()
-    result = oak.update(battle, durations, 0, 0)
 
-    print(oak.result_type(result))
+    ps_battle = PSBattle("", PSPlayer(), PSPlayer())
+    ps_battle.us = "p1"
+
+    result, msg = oak.log.update_(battle, durations, 0, 0)
+    for line in msg:
+        ps_battle.update(line)
+    ps_battle.process_msg_lines_and_clear()
 
     while not oak.result_type(result):
         p1_choices, p2_choices = oak.choices(battle, result)
         c1 = random.choice(p1_choices)
         c2 = random.choice(p2_choices)
         result, msg = oak.log.update_(battle, durations, c1, c2)
-        print(msg)
+
+        for line in msg:
+            ps_battle.update(line)
+        ps_battle.process_msg_lines_and_clear()
+
+        print(oak.battle_string(ps_battle.public, ps_battle.durations))
+
+        exit()
+
 
 def main():
     rollout_random_battle_with_log()
+
 
 if __name__ == "__main__":
     main()
