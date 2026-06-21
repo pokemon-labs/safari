9bf1c5f0a67ee81395f1989666083af5ea498a7c
open-cover <admin@opencover.net>
2026-06-21 15:36:56 -0600

    banned moves handled with args now, start support for matching key

--- tests/client.py
+++ tests/client.py
@@ -32,38 +32,63 @@ parser.add_argument(
     type=int,
     default=0,
 )
-
-
-BANNED_MOVES = tuple(
-    oak.id_to_move(x)
-    for x in (
-        # ts is fucking annoying i dont understand pp deduction currently
-        "metronome",
-        "mirrormove",
-        # if a mimic'd move is already present the mon can have it twice and we can't tell which one was used/pp deducted
-        "mimic",
-        # These seem done we are only hitting cases where libpkmn leaks that binding has ended
-        "bind",
-        "wrap",
-        "firespin",
-        "clamp",
-        # Not handling first-rage-on-immune correctly (don't set Rage, but immune message comes after)
-        "rage",
-        # this seems to be done, but we don't compare bide damage when min_damage should be used
-        "bide",
-        # These are done except flinch silently clears recharge. We can correct this with the request object (TODO clear recharge if can attack) but only for p1
-        "headbutt",
-        "stomp",
-        "bite",
-        "lowkick",
-    )
+parser.add_argument(
+    "--banned-moves",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--no-flinch",
+    action="store_true",
+)
+parser.add_argument(
+    "--no-binding",
+    action="store_true",
+)
+parser.add_argument(
+    "--no-pp",
+    action="store_true",
 )
 
+TEAMS = oak.load_teams("/home/user/teams150")
 
-def fill_side_randomly(side: oak.Side, RNG):
+
+def get_banned_moves(args) -> list[int]:
+    banned = []
+    if args.no_flinch:
+        banned += ["headbutt", "stomp", "bite", "lowkick", "boneclub"]
+    if args.no_binding:
+        banned += ["wrap", "bind", "clamp", "firespin"]
+    if args.no_pp:
+        banned += ["metronome", "mimic", "mirrormove"]
+    if args.banned_moves:
+        banned += args.banned_moves.split(",")
+    return [oak.id_to_move(m) for m in banned]
+
+
+def get_match_keys(args):
+    battle = oak.Battle()
+    durations = oak.Durations()
+
+    for i in range(2):
+        side = battle.side(i)
+        vol = side.active.volatiles()
+        vol.substitute_hp = 255
+
+    # opp stuff
+    battle.side(1).pokemon(0).hp = 15
+    battle.side(1).last_selected_move = 1
+
+    battle.last_damage = 1
+
+    return battle, durations
+
+
+def fill_side_randomly(RNG, side: oak.Side, banned_moves: list[int]):
     species_list = list(range(1, 150))
     # species_list.remove(oak.id_to_species("ditto"))
     RNG.shuffle(species_list)
+    order = [0 for _ in range(6)]
     for i, species in enumerate(species_list[:6]):
         s = oak.Set()
         s.species = species
@@ -71,18 +96,23 @@ def fill_side_randomly(side: oak.Side, RNG):
         oak.fill_random_moveset(s, RNG.randint(0, 2**32 - 1))
         moves = list(s.moves)
         for m in range(4):
-            if moves[m] in BANNED_MOVES:
+            if moves[m] in banned_moves:
                 moves[m] = 0
         moves = sorted(moves, reverse=True)
         s.moves = moves
         oak.complete_pokemon_from_set(side.pokemon(i), s)
-    side.order = list(range(1, 7))
+        order[i] = i + 1
+    side.order = order
+    # team = RNG.choice(TEAMS)
+    # for i, s in enumerate(team):
+    #     oak.complete_pokemon_from_set(side.pokemon(i), s)
+    # side.order = list(range(1, 7))
 
 
-def get_battle(RNG) -> oak.Battle:
+def get_battle(RNG, banned_moves: list[int]) -> oak.Battle:
     battle = oak.Battle()
-    fill_side_randomly(battle.side(0), RNG)
-    fill_side_randomly(battle.side(1), RNG)
+    fill_side_randomly(RNG, battle.side(0), banned_moves)
+    fill_side_randomly(RNG, battle.side(1), banned_moves)
     return battle
 
 
@@ -99,10 +129,14 @@ class Messages:
 
 def scan():
     args = parser.parse_args()
-    for seed in range(args.start, 10000):
+    battle_key, durations_key = get_match_keys(args)
+    banned_moves = get_banned_moves(args)
+
+    for seed in range(args.start, 100000):
         print(f"SEED: {seed}")
         RNG = random.Random(seed)
-        battle = get_battle(RNG)
+
+        battle = get_battle(RNG, banned_moves)
         durations = oak.Durations()
         ps_battle = PSBattle("", PSPlayer(), PSPlayer())
         ps_battle.us = "p1"
@@ -117,37 +151,27 @@ def scan():
             p1_choices, p2_choices = oak.choices(battle, result)
             c1 = RNG.choice(p1_choices)
             c2 = RNG.choice(p2_choices)
-
             messages.append(
                 f"{oak.choice_label(battle.side(0), c1)} {oak.choice_label(battle.side(1), c2)}"
             )
+
+            # last_selected_move
             active = ps_battle.public.side(0).active
             vol = active.volatiles()
             is_forced = vol.charging or vol.recharging or vol.rage or vol.thrashing
             c1_type = c1 & 3
             c1_data = c1 >> 2
-            if c1_type == 1 and c1_data > 0 and not is_forced:
-                ms = battle.side(0).active.move(c1_data - 1)
-                ps_battle.public.side(0).last_selected_move = ms.id
+            if c1_type == 1:
+                if c1_data > 0 and not is_forced:
+                    ms = battle.side(0).active.move(c1_data - 1)
+                    ps_battle.public.side(0).last_selected_move = ms.id
+                elif c1_data == 0:
+                    ps_battle.public.side(0).last_selected_move = oak.id_to_move(
+                        "struggle"
+                    )
 
             # UPDATE
             result, msg = oak.log.update(battle, durations, c1, c2, PLAYER)
-
-            for line in msg:
-                ps_battle.update(line)
-                messages.append(line)
-            # active move id's are public after transform
-            # ps_battle.truth = PSBattle.Truth()
-            ps_battle.init_truth()
-            ps_battle.truth.battle = battle
-            ps_battle.truth.durations = durations
-            ps_battle.process_msg_lines_and_clear()
-            matches, reason = oak.log.compare_battles(
-                ps_battle.public,
-                ps_battle.durations,
-                battle,
-                durations,
-            )
             messages.append(
                 "Actual battle:\n"
                 + oak.battle_string(
@@ -155,12 +179,33 @@ def scan():
                     durations,
                 )
             )
+
+            for line in msg:
+                ps_battle.update(line)
+                messages.append(line)
+
+            # This is currently just for our active.moves after a transform
+            ps_battle.init_truth()
+            ps_battle.truth.battle = battle
+            ps_battle.truth.durations = durations
+
+            # LOG UPDATE
+            ps_battle.process_msg_lines_and_clear()
             messages.append(
                 "Client battle:\n"
                 + oak.battle_string(
                     ps_battle.public,
                     ps_battle.durations,
                 )
+            )
+
+            matches, reason = oak.log.compare_battles(
+                ps_battle.public,
+                ps_battle.durations,
+                battle,
+                durations,
+                battle_key,
+                durations_key,
             )
 
             if not matches:
